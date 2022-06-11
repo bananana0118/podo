d19da1e99e3e7295ddada3a50e1b84740600a3b9
YeaJeong <bananana0118@gmail.com>
2022-06-12 00:17:44 +0900

    feature:포도알 css수정

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <div id="podo">
+                <div id="subPodo"></div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
